60e1a11d85cc61d28569e7e75d882839486f2ef9
Raquel Sylos <raquelsylos@gmail.com>
2025-02-12 11:10:28 -0300

    rm and add new credentials

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,3 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-# Ensuring the SECRET_KEY_BASE is loaded correctly
-Rails.application.config.secret_key_base = ENV['SECRET_KEY_BASE']
